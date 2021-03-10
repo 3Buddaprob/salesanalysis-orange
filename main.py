f4111aafd43463ea06c430884544cf39f2e042ff
3Buddaprob <61561829+3Buddaprob@users.noreply.github.com>
2021-03-10 20:50:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import csv
+
+
+def read_data() -> object:
+    data = []
+
+    with open('sales.csv', 'r') as sales_csv:
+        spreadsheet = csv.DictReader(sales_csv)
+        for row in spreadsheet:
+            data.append(row)
+    return data
+
+
+def run():
+    data = read_data()
+    sales = []
+
+    for row in data:
+        sale = int(row['sales'])
+        sales.append(sale)
+
+    total = sum(sales)
+    print(f'total sales:{total}')
+
+
+run()
