fcdc130eb281c2dba7141a8a386bffce5c991841
3Buddaprob <61561829+3Buddaprob@users.noreply.github.com>
2021-03-16 20:19:45 +0000

    hc

--- combine.py
+++ combine.py
@@ -1,8 +1,10 @@
 import csv
-from typing import List
+import matplotlib.pyplot as plot
+import seaborn as sns
+
 
 data = []
-with open('sales.csv', 'r') as file:
+with open('salesv2.csv', 'r') as file:
     reader = csv.DictReader(file)
     for row in reader:
         data.append(row)
@@ -10,16 +12,29 @@ with open('sales.csv', 'r') as file:
 sales = []
 for row in data:
     sale = int(row['sales'])
-    sales.append(sale)
+    sales.append(sale) #collect sales
 
 expenditures = []
-
 for row in data:
     expenditure = int(row['expenditure'])
     expenditures.append(expenditure) #collect expenditures
-    
+
+months = []
+for row in data:
+    month = (row['month_date'])
+    months.append(month) #collect months
+
+def graphing():
+    x=months
+    y=sales
+    ax=sns.stripplot(x,y);
+    ax.set(xlabel='months', ylabel='sales')
+    plt.title('Graph')
+    plt.show()
+
+graphing()
 total_s = sum(sales)
 total_ex = sum(expenditures)
 
 print(f'total sales:{total_s}') #output total sales
-print(f'total expenditures: {total_ex}')
+print(f'total expenditures: {total_ex}') #output total expenditures
