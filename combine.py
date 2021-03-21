1a859ad356fc8bea9017c3743a11e3c26dd0455a
3Buddaprob <61561829+3Buddaprob@users.noreply.github.com>
2021-03-21 20:36:40 +0000

    Merged

--- combine.py
+++ combine.py
@@ -32,6 +32,51 @@ def graphing():
     plt.title('Graph')
     plt.show()
 
-graphing()
 total_s = sum(sales)
 total_ex = sum(expenditures)
+
+with open('names.csv', 'w+') as csvfile:
+    csvfile.write(f'total sales:{total_s} \n total expenditures: {total_ex}')
+
+data2= []
+def read_data() -> object: #read data
+
+    with open('sales2.csv', 'r') as sales2_csv:
+        spreadsheet = csv.DictReader(sales2_csv)
+        for row in spreadsheet:
+            data2.append(row)
+    return data2
+
+max_sale=max(sales)
+min_sale=min(sales)
+
+# for row in data:
+#     print ("In row %s, Max = %s" % (index, max(row))
+#
+# for row in data:
+#     print ("In row %s, Min = %s" % (index, min(row))
+
+max_sale_row={}
+def search(sales):
+    for x in data:
+        if int(x['sales']) == int(sales):
+            max_sale_row.update(x)
+
+search(max_sale)
+print(max_sale_row)
+max_sale_yr = max_sale_row['year']
+max_sale_mon = max_sale_row['month']
+print(max_sale_mon,max_sale_yr)
+
+for x in data:
+    a=x['sales']
+    b=x + 1['sales']
+    monthlychange = ((b-a)/b)*100
+    print(monthlychange)
+
+print(max_sale)
+print(min_sale)
+read_data()
+print(data2)
+graphing()
+
